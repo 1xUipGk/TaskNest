52fb3dddfd743b009bc26f805619d1fdbe0dd65b
1xUipGk <instagraminarabic123@gmail.com>
2024-11-02 15:32:16 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB6tkZjsM5I4OkG8SvCk6v5pyeM6nTzGT4",
+    authDomain: "task-manager-87577.firebaseapp.com",
+    projectId: "task-manager-87577",
+    storageBucket: "task-manager-87577.firebasestorage.app",
+    messagingSenderId: "949071438979",
+    appId: "1:949071438979:web:d2a0056c95b9e02e90e7b6",
+    measurementId: "G-HMZ54Z29EZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export { auth };
